0434c1922e15b4b96a5d82fb22c729fec79d7f51
frank_wu <frank_wu@preludecorp.com>
2014-08-20 16:39:44 +0800

    mysql docker file

---
+++ mysql/startup.sh
@@ -0,0 +1,14 @@
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+    mysql_install_db
+
+    /usr/bin/mysqld_safe &
+    sleep 10s
+
+    echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'mysql_admin$' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
+    killall mysqld
+    sleep 10s
+fi
+
+/usr/bin/mysqld_safe
